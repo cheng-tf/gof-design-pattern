c8ab014e939a9089717b4ed46b0fdf5f89ac0640
chengtf <chengtf94@126.com>
2018-06-30 20:30:34 +0800

    解释器模式

---
+++ _19-interpreter/src/main/java/cn/edu/bupt/opensource/example/AbstractExpression.java
@@ -0,0 +1,48 @@
+package cn.edu.bupt.opensource.example;
+
+/**
+ * <p>Title: AbstractExpression</p>
+ * <p>Description: </p>
+ * <p>Company: bupt.edu.cn</p>
+ * <p>Created: 2018-06-30 16:11</p>
+ * @author ChengTengfei
+ * @version 1.0
+ *
+ * 解释器模式
+ *  定义：给定一个语言，定义它的文法的一种表示，并定义一个解释器，这个解释器使用该表示来解释语言中的句子。
+ *  角色：
+ *      1）AbstractExpression：解释器接口
+ *      2）TerminalExpression：终结符解释器
+ *      3）NonterminalExpression：非终结符解释器
+ *      4）Context：上下文
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *  本质：分离实现，解释执行
+ *
+ *
+ *
+ */
+public class AbstractExpression {
+}
