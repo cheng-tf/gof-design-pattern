5191a7df954fe020b7e8375a69007bdb567cdc9f
chengtf <chengtf94@126.com>
2018-07-01 12:39:12 +0800

    访问者模式

---
+++ _23-visitor/src/main/java/cn/edu/bupt/opensource/example/Visitor.java
@@ -0,0 +1,48 @@
+package cn.edu.bupt.opensource.example;
+
+/**
+ * <p>Title: Visitor</p>
+ * <p>Description: </p>
+ * <p>Company: bupt.edu.cn</p>
+ * <p>Created: 2018-07-01 12:25</p>
+ * @author ChengTengfei
+ * @version 1.0
+ *
+ * 访问者模式
+ *  定义：表示一个作用于某对象结构中的各元素的操作。可以在不改变各元素的类的前提下，定义作用于这些元素的新操作。
+ *  角色：
+ *      1）Visitor
+ *      2）ConcreteVisitor
+ *      3）Element
+ *      4）ConcreteElement
+ *      5）ObjectStructure
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *  本质：预留通路，回调实现。
+ *
+ *
+ *
+ *
+ */
+public class Visitor {
+}
